730439c8569bbbf713d6fcbf640de43ca426a751
drmats <drmats@users.noreply.github.com>
2014-02-01 16:47:02 +0100

    Comments and file header.

--- glspiders.c
+++ glspiders.c
@@ -1,6 +1,33 @@
 /**
- * ...
+ *  glspiders v. 0.0.1
+ *
+ *  Copyright (c) 2014, drmats
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+ *  OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ *  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#define _ISOC99_SOURCE
 
 #include <GL/glut.h>
 #include <stdlib.h>
@@ -10,39 +37,22 @@
 
 
 /**
- *  Globals.
+ *  Global variables.
  */
-GLfloat camera_rotation_angle;
-GLfloat floor_position;
-GLdouble spider_leg_angle;
-GLdouble ant_angle;
-GLdouble spider2_movement;
-GLdouble spider3_angle;
+GLfloat camera_rotation_angle = 0.0;
+GLfloat floor_position = -5.0;
+GLdouble spider_leg_angle = 0.0;
+GLdouble fly_angle = 0.0;
+GLdouble spider2_movement = 0.0;
+GLdouble spider3_angle = 0.0;
 
 
 
 
 /**
- *  ...
+ *  Spider leg drawing routine.
  */
-void init (void) {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glShadeModel(GL_SMOOTH);
-    camera_rotation_angle = 0.0;
-    floor_position = -5.0;
-    spider_leg_angle = 0.0;
-    ant_angle = 0.0;
-    spider2_movement = 0.0;
-    spider3_angle = 0.0;
-}
-
-
-
-
-/**
- *  ...
- */
-void leg (GLdouble o, GLdouble s) {
+void draw_leg (GLdouble o, GLdouble s) {
     int a = 0;
 
     glPushMatrix();
@@ -76,9 +86,9 @@ void leg (GLdouble o, GLdouble s) {
 
 
 /**
- *  ...
+ *  Spider drawing routine.
  */
-void tarantula (void) {
+void draw_spider (void) {
     int i;
 
     glPushMatrix();
@@ -104,7 +114,7 @@ void tarantula (void) {
             glRotatef(80.0, 0.0, 1.0, 0.0);
             for (i = 0;  i < 3;  i++) {
                 glRotatef(-40.0, 0.0, 1.0, 0.0);
-                leg(i * 120.0, 1.0);
+                draw_leg(i * 120.0, 1.0);
             }
         glPopMatrix();
 
@@ -114,7 +124,7 @@ void tarantula (void) {
             glRotatef(-80.0, 0.0, 1.0, 0.0);
             for (i = 0;  i < 3;  i++) {
                 glRotatef(40.0, 0.0, 1.0, 0.0);
-                leg((GLdouble)((((i + 1) * 120) % 360) + 60), -1.0);
+                draw_leg((GLdouble)((((i + 1) * 120) % 360) + 60), -1.0);
             }
         glPopMatrix();
     glPopMatrix();
@@ -124,7 +134,7 @@ void tarantula (void) {
 
 
 /**
- *  ...
+ *  Frame drawing function.
  */
 void display (void) {
     int i;
@@ -207,7 +217,7 @@ void display (void) {
         /* spider on conveyor belt */
         glPushMatrix();
             glTranslatef(0.0, 0.9, 0.0);
-            tarantula();
+            draw_spider();
         glPopMatrix();
 
         /* flies */
@@ -215,7 +225,7 @@ void display (void) {
             glPushMatrix();
                 glTranslatef(0.0, 1.5, 4.0);
                 glRotatef(
-                    ant_angle * (i+1) * 0.5,
+                    fly_angle * (i+1) * 0.5,
                     (GLdouble)((i+1) % 2),
                     (GLdouble)((i+4) % 2),
                     (GLdouble)((i+8) % 2)
@@ -232,7 +242,7 @@ void display (void) {
         glRotatef(spider3_angle, 0.0, 1.0, 0.0);
         glTranslatef(0.0, 0.0, 5.0);
         glRotatef(90.0, 0.0, 1.0, 0.0);
-        tarantula();
+        draw_spider();
     glPopMatrix();
 
     /* 2nd spider running around */
@@ -241,7 +251,7 @@ void display (void) {
         glRotatef(-spider3_angle, 0.0, 1.0, 0.0);
         glTranslatef(0.0, 0.0, 6.0);
         glRotatef(-90.0, 0.0, 1.0, 0.0);
-        tarantula();
+        draw_spider();
     glPopMatrix();
 
     /* 3rd spider running around */
@@ -250,21 +260,21 @@ void display (void) {
       glRotatef(-spider3_angle, 0.0, 1.0, 0.0);
       glTranslatef(0.0, 0.0, 4.0);
       glRotatef(-90.0, 0.0, 1.0, 0.0);
-      tarantula();
+      draw_spider();
     glPopMatrix();
 
     /* 1st spider running from "far away" */
     glPushMatrix();
         glTranslatef(-75.0 + spider2_movement, 0.0, -12.0);
         glRotatef(90.0, 0.0, 1.0, 0.0);
-        tarantula();
+        draw_spider();
     glPopMatrix();
 
     /* 2nd spider running from "far away" */
     glPushMatrix();
         glTranslatef(-12, 0.0, 75.0 - spider2_movement);
         glRotatef(180, 0.0, 1.0, 0.0);
-        tarantula();
+        draw_spider();
     glPopMatrix();
 
     /* squiggle */
@@ -290,7 +300,7 @@ void display (void) {
 
 
 /**
- *  ...
+ *  Periodically called for variables update.
  */
 void animation (void) {
     camera_rotation_angle += 0.9;
@@ -313,9 +323,9 @@ void animation (void) {
         spider_leg_angle -= 360.0;
     }
 
-    ant_angle += 5;
-    if (ant_angle > 7 * 360.0) {
-        ant_angle -= 8 * 360.0;
+    fly_angle += 5;
+    if (fly_angle > 7 * 360.0) {
+        fly_angle -= 8 * 360.0;
     }
 
     spider3_angle += 1.5;
@@ -330,7 +340,7 @@ void animation (void) {
 
 
 /**
- *  ...
+ *  Viewport resize handler.
  */
 void reshape (int w, int h) {
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
@@ -346,7 +356,7 @@ void reshape (int w, int h) {
 
 
 /**
- *  ...
+ *  Keyboard handler.
  */
 void keyboard (unsigned char key, int x, int y) {
     switch (key) {
@@ -360,20 +370,21 @@ void keyboard (unsigned char key, int x, int y) {
 
 
 /**
- *  ...
+ *  Program entry point.
  */
 int main (int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800, 450);
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
-    init();
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
     glutIdleFunc(animation);
     glutFullScreen();
     glutMainLoop();
-    return 0;
+
+    exit(0);
 }
